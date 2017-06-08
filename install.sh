cc40b3a34c93d479f661da3e7e797a625bc4bed6
Kamoor <rkamoor@gmail.com>
2017-06-07 21:30:45 -0400

    adding a simple install step

---
+++ install.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+sudo apt install awscli
+sudo apt-get install openjdk-8-jre-headless
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.4.0.deb
+sudo dpkg -i elasticsearch-5.4.0.deb
+sudo mkdir /es
+sudo mkdir /es/data
+sudo mkdir es/logs
+sudo chown -R elasticsearch:elasticsearch /es/
+
+wget https://raw.githubusercontent.com/kamoor/elastic-search-config/master/elasticsearch.yml
+wget https://raw.githubusercontent.com/kamoor/elastic-search-config/master/jvm.options
+sudo cp ./elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+sudo cp ./jvm.options /etc/elasticsearch/jvm.options
+
+sudo service elasticsearch start
