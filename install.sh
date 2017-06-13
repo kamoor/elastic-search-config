2e4f2c2950193bd102477706c306e716126a8d3f
Kamoor <rkamoor@gmail.com>
2017-06-13 12:20:40 -0400

    added master node as command line

--- install.sh
+++ install.sh
@@ -1,19 +1,26 @@
 # wget https://raw.githubusercontent.com/kamoor/elastic-search-config/master/install.sh
 # chmod 755 ./install.sh
-# ./install.sh
-sudo apt-get update
-sudo apt install awscli
-sudo apt-get install openjdk-8-jre-headless
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.4.0.deb
-sudo dpkg -i elasticsearch-5.4.0.deb
-sudo mkdir /es
-sudo mkdir /es/data
-sudo mkdir /es/logs
-sudo chown -R elasticsearch:elasticsearch /es/
+# ./install.sh MASTER_NODE_IP
 
-wget -O elasticsearch.yml https://raw.githubusercontent.com/kamoor/elastic-search-config/master/elasticsearch.yml
-wget -O jvm.options https://raw.githubusercontent.com/kamoor/elastic-search-config/master/jvm.options
-sudo cp ./elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
-sudo cp ./jvm.options /etc/elasticsearch/jvm.options
+FIRST_NODE=$1
+#sudo apt-get update
+#sudo apt install awscli
+#sudo apt-get install openjdk-8-jre-headless
+#wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.4.0.deb
+#sudo dpkg -i elasticsearch-5.4.0.deb
+#sudo mkdir /es
+#sudo mkdir /es/data
+#sudo mkdir /es/logs
+#sudo chown -R elasticsearch:elasticsearch /es/
 
-sudo service elasticsearch start
+#wget -O elasticsearch.yml https://raw.githubusercontent.com/kamoor/elastic-search-config/master/elasticsearch.yml
+#wget -O jvm.options https://raw.githubusercontent.com/kamoor/elastic-search-config/master/jvm.options
+
+#replace master node
+echo $FIRST_NODE
+sed -i -e s/MASTER_NODE/$FIRST_NODE/g elasticsearch.yml
+
+#sudo cp ./elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+#sudo cp ./jvm.options /etc/elasticsearch/jvm.options
+
+#sudo service elasticsearch start
