121875503f7382dde20ccf84a03fb4a83644e733
Ratheesh Kamoor <rkamoor@gmail.com>
2017-07-20 00:02:00 -0400

    Update install-controller.sh

--- install-controller.sh
+++ install-controller.sh
@@ -1,4 +1,4 @@
-# wget https://raw.githubusercontent.com/kamoor/elastic-search-config/master/install.sh
+# wget https://raw.githubusercontent.com/kamoor/elastic-search-config/master/install-controller.sh
 # chmod 755 ./install.sh
 # ./install.sh MASTER_NODE_IP
 
@@ -6,13 +6,14 @@ sudo apt-get update
 sudo apt install awscli
 sudo apt-get install openjdk-8-jre-headless
 
-wget https://artifacts.elastic.co/downloads/kibana/kibana-5.4.1-linux-x86.tar.gz
-tar -xzf kibana-5.4.1-linux-x86.tar.gz
+wget https://artifacts.elastic.co/downloads/kibana/kibana-5.4.1-linux-x86_64.tar.gz
+tar -xzf kibana-5.4.1-linux-x86_64.tar.gz 
 
-wget get kibana.yml
-cp kibana.yl 
+#./kibana-5.5.0-linux-x86_64/bin/kibana-plugin install x-pack
 
-./kibana-5.4.1-linux-x86_64/bin/kibana-plugin install x-pack
+echo "server.host: 0.0.0.0" >> ./kibana-5.4.1-linux-x86_64/config/kibana.yml 
+echo "server.port: 9200" >> ./kibana-5.4.1-linux-x86_64/config/kibana.yml 
+echo "elasticsearch.url: http://$1:9200" >> ./kibana-5.4.1-linux-x86_64/config/kibana.yml 
 
 sudo ./kibana-5.4.1-linux-x86_64/bin/kibana &
 
